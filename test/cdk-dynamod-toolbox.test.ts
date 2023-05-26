4aa843955a5ad72e76ff32f1f926df8695ddf58a
Andreas Hailu <andreashailu@gmail.com>
2023-05-26 14:07:37 -0400

    Initial commit

---
+++ test/cdk-dynamod-toolbox.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDynamodToolbox from '../lib/cdk-dynamod-toolbox-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-dynamod-toolbox-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDynamodToolbox.CdkDynamodToolboxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
