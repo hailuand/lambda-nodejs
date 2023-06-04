33c7509432027e2d776adf28e1af0c01a44e09b8
Andreas Hailu <andreashailu@gmail.com>
2023-06-04 14:18:11 -0400

    AVJ schema management

--- src/lambda/get-customer.ts
+++ src/lambda/get-customer.ts
@@ -9,15 +9,29 @@ const ajv = new Ajv({
 const validateCustomer = ajv.compile(CustomerSchema);
 
 export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const body = JSON.parse(event.body as string);
+    let body;
+    
+    try {
+        body = JSON.parse(event.body as string);
+    }
+    catch(err) {
+        const errResp = {
+            message: `Invalid Customer schema: ${err}`
+        }
+        return {
+            statusCode: 400,
+            body: JSON.stringify(errResp)
+        }
+    }
+    
     console.log(`Raw: ${JSON.stringify(body, undefined, 2)}`);
 
     if(!validateCustomer(body)) {
-        const errors: string[] = [];
-        validateCustomer.errors?.forEach(e => errors.push(e.message as string));
-        console.error(JSON.stringify(errors));
+        // const errors: string[] = [];
+        // validateCustomer.errors?.forEach(e => errors.push(e.));
+        console.error(JSON.stringify(validateCustomer.errors));
         const errResp = {
-            message: `Invalid Customer schema: [${errors.join(", ")}]`
+            message: validateCustomer.errors
         };
         return {
             statusCode: 400,
