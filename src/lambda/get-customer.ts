5a44535c05f3c6afb8ae0f1524b2aed8919bc83a
Andreas Hailu <andreashailu@gmail.com>
2023-06-07 20:13:52 -0400

    Logging examples

--- src/lambda/get-customer.ts
+++ src/lambda/get-customer.ts
@@ -1,14 +1,46 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { CustomerEntity } from "./customer-entity";
 import { Customer, CustomerSchema } from "../../models/customer";
 import Ajv from "ajv";
+import winston = require("winston");
+import { pino } from "pino";
 
 const ajv = new Ajv({
     allErrors: true
 });
 const validateCustomer = ajv.compile(CustomerSchema);
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+// const logger = pino({
+//     timestamp: pino.stdTimeFunctions.isoTime,
+//     base: undefined, // removes pid + hostname
+//     formatters: {
+//         level(level) { // uses text instead of numerical level representation
+//             return {level};
+//         }
+//     }
+// });
+// Log even uncaught exceptions
+process.on('uncaughtException', (err) => {
+    // logger.fatal(err);
+    logger.crit(err);
+    process.exit(1);
+  });
+  
+  process.on('unhandledRejection', (err) => {
+    // logger.fatal(err);
+    logger.crit(err);
+    process.exit(1);
+  });
 
-export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+    logger.defaultMeta = {requestId: context.awsRequestId};
+    
     let body;
     
     try {
@@ -23,13 +55,13 @@ export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayPro
             body: JSON.stringify(errResp)
         }
     }
-    
-    console.log(`Raw: ${JSON.stringify(body, undefined, 2)}`);
+    logger.info("Testing testing 123!");
+    logger.info(`Raw: ${JSON.stringify(body, undefined, 2)}`);
 
     if(!validateCustomer(body)) {
         // const errors: string[] = [];
         // validateCustomer.errors?.forEach(e => errors.push(e.));
-        console.error(JSON.stringify(validateCustomer.errors));
+        logger.error(JSON.stringify(validateCustomer.errors));
         const errResp = {
             message: validateCustomer.errors
         };
@@ -39,7 +71,7 @@ export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayPro
         };
     }
     const customer: Customer = Object.assign(new Customer(), body);
-    console.log(`Parsed: ${JSON.stringify(customer, undefined, 2)}`);
+    logger.info(`Parsed: ${JSON.stringify(customer, undefined, 2)}`);
 
     return {
         statusCode: 200,
